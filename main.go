550768f336d94dbb0882458c41dcc696c2250ee6
dangviethung096 <dangviethung096@gmail.com>
2024-01-22 22:46:17 +0700

    update main

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"read_book_webs/controller"
 	"read_book_webs/route"
 )
 
+var port = flag.Int64("port", 8080, "listen port")
+
 func main() {
+	flag.Parse()
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./html/assets"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./html/css"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./html/js"))))
@@ -16,6 +20,9 @@ func main() {
 	http.HandleFunc("/login", route.RouteLoginPage)
 	http.HandleFunc("/", route.RouteHomePage)
 
-	fmt.Println("Listen and serve 80")
-	http.ListenAndServe(":80", nil)
+	fmt.Printf("Listen and serve %d\n", *port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	if err != nil {
+		fmt.Printf("Listen port %d fail: %s", *port, err.Error())
+	}
 }
