fc47790c5a4661ea9ead3e30f749b71471053e69
Hung Dang Viet <dangviethung096@gmail.com>
2024-02-11 10:26:36 +0700

    update to 14/02

--- html/js/script.js
+++ html/js/script.js
@@ -1,6 +1,6 @@
 function getDayLeft() {
     var now = new Date();
-    var tetHoliday = new Date(2024, 1, 10);
+    var tetHoliday = new Date(2024, 1, 14);
     var differenceInMilliseconds = tetHoliday - now;
     var differenceInDays = Math.ceil(differenceInMilliseconds / (1000 * 60 * 60 * 24));
 
